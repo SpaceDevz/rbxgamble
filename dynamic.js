ab72292e87295430ee8cfc7fe4fcbfc8bc466e92
brody critchlow <brody@tntsplash.net>
2021-07-08 23:45:52 -0700

    First commit

---
+++ dynamic.js
@@ -0,0 +1,33 @@
+function createElementHtml(){
+    document.write("<!DOCTYPE HTML>");
+    document.write("<html>");
+    document.write("    <head>");
+    document.write("        <title>Test<\/title>");
+    document.write("    <\/head>");
+    document.write("    <body>");
+    document.write("        <h1> Game Data: \n </h1> ");
+    document.write("<h2>    items:   </h2>");
+    document.write("<h2>    UserID:   </h2>");
+    document.write("<h2>    TimeRemaning:   </h2>");
+    document.write("<h2>    GameID:   </h2>");
+    document.write("    <\/body>");
+    document.write("<\/html>");
+}
+  //usage
+
+function creategame(items, user) {
+    // create index.html element
+    // send items to bot
+}
+
+function joingame(gameid, user) {
+    // create index.html element
+    // send items to bot
+}
+
+function gametimed(gameid) {
+    // if time is over 15m
+    // delete game element
+    // send back items
+}
+
